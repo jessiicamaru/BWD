351ec88311d2776fb08bad10207b1b47f796b921
jessiicamaru <Dandrew2407@gmail.com>
2024-05-09 23:35:35 +0700

    update

--- src/components/Letters/index.jsx
+++ src/components/Letters/index.jsx
@@ -6,10 +6,10 @@ export default function Index() {
             <div className={styles.letterContainerLeft}>Những bức thư từ chiến trường</div>
             <div className={styles.letterContainerRight}>
                 <p>
-                    "Mẹ cứ yên tâm đừng lo nghĩ gì về con nhiều quá mà ảnh hưởng sức khoẻ mẹ ạ! Mà mẹ thì già yếu rồi, làm việc bình thường thôi đừng
-                    quá sức mẹ nhé và buổi tối mẹ đi ngủ sớm đừng muộn mà ảnh hưởng đến sức khoẻ. Còn con hiện nay thì không thiếu thốn gì cả, chỉ
-                    buồn thôi cho nên mẹ chỉ cần gửi thư cho con và không cần gửi tiền hay bánh kẹo gì cả khi nào thiếu thốn gì con sẽ biên thư về
-                    sau."
+                    &quot;Mẹ cứ yên tâm đừng lo nghĩ gì về con nhiều quá mà ảnh hưởng sức khoẻ mẹ ạ! Mà mẹ thì già yếu rồi, làm việc bình thường thôi
+                    đừng quá sức mẹ nhé và buổi tối mẹ đi ngủ sớm đừng muộn mà ảnh hưởng đến sức khoẻ. Còn con hiện nay thì không thiếu thốn gì cả,
+                    chỉ buồn thôi cho nên mẹ chỉ cần gửi thư cho con và không cần gửi tiền hay bánh kẹo gì cả khi nào thiếu thốn gì con sẽ biên thư về
+                    sau.&quot;
                 </p>
             </div>
         </div>
