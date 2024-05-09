7c54055b3c905b104b87ba2c4f188a83f3b67c4d
jessiicamaru <Dandrew2407@gmail.com>
2024-05-09 23:20:51 +0700

    fix "''"

--- src/components/Letters/index.jsx
+++ src/components/Letters/index.jsx
@@ -4,7 +4,7 @@ export default function Index() {
     return (
         <div className={styles.letterContainer}>
             <div className={styles.letterContainerLeft}>Những bức thư từ chiến trường</div>
-            <div className={styles.letterContainerRight} style={{ fontFamily: "'Kolker Brush', cursive !important" }}>
+            <div className={styles.letterContainerRight} style={{ fontFamily: '"Kolker Brush", cursive !important' }}>
                 <p>
                     "Mẹ cứ yên tâm đừng lo nghĩ gì về con nhiều quá mà ảnh hưởng sức khoẻ mẹ ạ! Mà mẹ thì già yếu rồi, làm việc bình thường thôi đừng
                     quá sức mẹ nhé và buổi tối mẹ đi ngủ sớm đừng muộn mà ảnh hưởng đến sức khoẻ. Còn con hiện nay thì không thiếu thốn gì cả, chỉ
