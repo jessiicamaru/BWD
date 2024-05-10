2fc99e8286f51523c501b81e349ae8a090ac1cfe
jessiicamaru <Dandrew2407@gmail.com>
2024-05-10 18:42:31 +0700

    add title

--- src/components/CardScroll/index.jsx
+++ src/components/CardScroll/index.jsx
@@ -67,6 +67,7 @@ export default function Index() {
 
     return (
         <div ref={container} className={styles.main}>
+            <h1 style={{ color: 'black', textAlign: 'center', paddingTop: '20px' }}>Những nét đẹp văn hóa </h1>
             {projects.map((project, i) => {
                 const targetScale = 1 - (projects.length - i) * 0.05;
                 return <Card key={`p_${i}`} i={i} {...project} progress={scrollYProgress} range={[i * 0.25, 1]} targetScale={targetScale} />;
